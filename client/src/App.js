8693c441f4bd391df154952c5565f4fc75023438
Alejandro Jacobo <57726734+alejandrojacobo00@users.noreply.github.com>
2021-03-18 22:11:25 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -4,45 +4,29 @@ import Members from './Members';
 import Donate from './Donate';
 import Contact from './Contact';
 import NavBar from './NavBar';
-import { Route, Link } from "react-router-dom";
+import Home from './Home';
+import { Route, Link, BrowserRouter } from "react-router-dom";
 
 
 function App() {
   return (
+  <BrowserRouter>
     <div>
-      {/* This was in the HTML head file, might need to change the structure of app.js for that */}       
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-
-
-      {/* Left side */} 
-      <div className= "split left grid-container">
+        <NavBar/> 
       
-        <main className= "centered"> 
-          {/* <img className = "FrontCover " src = "../Images/Capture.JPG" alt = " team">  </img>   */}           
-          <a href="index.html">Tone Def Acapellla</a>     
-          <NavBar/>         
-          <Route exact path="/Music" component={Music}/>
-          <Route exact path="/Members" component={Members}/>
-          <Route exact path="/Donate" component={Donate}/>
-          <Route exact path="/Contact" component={Contact}/>
-        </main>
-
-        <footer className = "icon_sepaeration">     
-          <a href="#" className="fa fa-facebook"></a>
-          <a href="#" className="fa fa-twitter"></a>
-          <a href="#" className="fa fa-instagram"></a>
-          <a href="#" className="fa fa-spotify"></a>      
-        </footer>
-      </div>
-    
-      {/* Right side */} 
-      <div className= "split right ">
-        <header><b>The Feed</b>  </header>
-      </div>
-
-
-    </div>
+        <Route exact path="/" component={Home}></Route> 
+        <Route exact path="/Music" component={Music}></Route> 
+        <Route exact path="/Members" component={Members}></Route> 
+        <Route exact path="/Donate" component={Donate}></Route> 
+        <Route exact path="/Contact" component={Contact}></Route>
+        
+         
+    </div>    
+    </BrowserRouter>
   );
+      
 }
 
 export default App;
+
+
---
+++ client/src/Home.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Music from './Music';
+import Members from './Members';
+import Donate from './Donate';
+import Contact from './Contact';
+import NavBar from './NavBar';
+import { Route, Link } from "react-router-dom";
+
+function Home(){
+    return(
+         
+        <div>  
+        {/* This was in the HTML head file, might need to change the structure of app.js for that */}       
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+  
+  
+        {/* Left side */} 
+        <div className= "split left grid-container">
+          
+  
+  
+          <main className= "centered"> 
+            {/* <img className = "FrontCover " src = "../Images/Capture.JPG" alt = " team">  </img>   */}           
+            <a href="index.html">Tone Def Acapellla</a>   
+  
+             
+            <Route exact path="/Music" component={Music}></Route> 
+            <Route exact path="/Members" component={Members}></Route> 
+            <Route exact path="/Donate" component={Donate}></Route> 
+            <Route exact path="/Contact" component={Contact}></Route>  
+                   
+          </main>
+  
+          <footer className = "icon_sepaeration">     
+            <a href="#" className="fa fa-facebook"></a>
+            <a href="#" className="fa fa-twitter"></a>
+            <a href="#" className="fa fa-instagram"></a>
+            <a href="#" className="fa fa-spotify"></a>      
+          </footer>
+        </div>
+      
+        {/* Right side */} 
+        <div className= "split right ">
+          <header><b>The Feed</b>  </header>
+        </div>
+  
+  
+      </div>
+   
+    );
+}
+
+export default Home;
