4ef6d84b8da2ca3e829dfafb132a8a72561e3706
Calvin Yang <cyang2@ufl.edu>
2021-03-23 14:18:04 -0400

    Mixed Luke's changes and Mine

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');   
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 
@@ -15,6 +16,12 @@ const PORT = process.env.PORT || 5000;  // When we have it hosted on Heroku, use
 console.log("Starting server");
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+__dirname = path.resolve();
+app.use(express.static(__dirname + '/client/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html');
+});
 
 // set up routes
 
