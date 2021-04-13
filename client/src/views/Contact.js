3540915f18997c5545fa6e317db6f54928f6e655
LiamStokes <75265103+LiamStokes@users.noreply.github.com>
2021-04-12 23:30:42 -0400

    Added google forms to contact page

--- client/src/views/Contact.js
+++ client/src/views/Contact.js
@@ -8,10 +8,12 @@ class Contact extends React.Component {
                     
                     <div class = "grid-item LeftSide"> 
                     <p id="blocktext"> Bookings </p>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSexfojZHA17_FWHB0c7Dz2KhQVIzAxNh8pNo-MOIOY4xd2eUw/viewform?embedded=true" width="710" height="1240" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                     </div>
 
                     <div class = "grid-item RightSide">
                     <p id="blocktext"> Contact Us </p>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfg_d1mFuTWJxtv2GEjo54dpZPKCOGgWNc0DsrjIONHCGRPow/viewform?embedded=true" width="710" height="976" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                     </div>
 
                 </div>
