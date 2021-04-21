ea8fa6fcae3be00d3285213a6587514a2e236ffd
LukeRouleau <lucasluke2000@gmail.com>
2021-04-21 14:11:58 -0400

    Fixed an error

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:3000' });
 
-API.interceptors.request.use(`/posts/`,(req) => {
+API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
       req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
     }
