a6505aa7bdc43627e0d5993acdb35dd5d4ccceea
Calvin Yang <cyang2@ufl.edu>
2021-04-21 13:27:41 -0400

    Added Alert on Invalid Credentials

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -10,6 +10,7 @@ export const signin = (formData, router) => async (dispatch) => {
     router.push('/');
   } catch (error) {
     console.log(error);
+    alert("Invalid Credentials");
   }
 };
 
@@ -22,5 +23,6 @@ export const signup = (formData, router) => async (dispatch) => {
     router.push('/');
   } catch (error) {
     console.log(error);
+    alert("Something went wrong!");
   }
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -27,7 +27,7 @@ router.post('/signin', async (req, res) => {
     const { email, password } = req.body;
     try {
         const oldUser = await User.findOne({ email });
-        if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+        if (!oldUser) return res.status(400).json({ message: "User doesn't exist" });
         const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
         if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
         const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, process.env.SECRET, { expiresIn: "1h" });
